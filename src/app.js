25b12c6d8ee513f8fa163b1b3a827b15b10ab630
ARosewarne <alex.rosewarne@gmail.com>
2022-01-03 14:04:54 +1100

    forecast js including the timestamp format

--- src/app.js
+++ src/app.js
@@ -21,6 +21,56 @@ function formatDate(timestamp){
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+  let date = new Date (timestamp * 1000);
+  let day = date.getDay();
+  let days = [ "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  
+  let forecastHTML = `<div class="row">`;
+  
+  forecast.forEach(function(forecastDay, index){
+    if (index < 6){
+      forecastHTML = forecastHTML + 
+      `
+        <div class="col-2">
+            <div class="forecast-date">
+              ${formatDay(forecastDay.dt)}
+            </div>
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
+            <div class="forecast-temperatures">
+              <span class="forecast-temp-max">${Math.round(forecastDay.temp.max)}° </span>
+              <span class="forecast-temp-min">${Math.round(forecastDay.temp.min)}° </span>
+            </div>
+        </div>
+      `;
+    }
+  });
+
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+} 
+
+function getForecast(coordinates){
+  let apiKey = "96fc9dbf0bd42fe281a341e984ec7160";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+};
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -43,8 +93,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-}
 
+  getForecast(response.data.coord);
+}
 
 function displayFahrentheitTemperature(event){
   event.preventDefault();
